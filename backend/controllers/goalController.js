97031c6f3be1988c683eef510723ea2aa52b0b04
hermanlyx <hermanlyx@gmail.com>
2022-02-19 19:54:34 +0800

    added custom error handler middleware and installed express-async-handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,28 +1,32 @@
 // Get goals
 // route GET /api/goals
 // access Private
-const getGoals = (req, res) => {
+const getGoals = async (req, res) => {
   res.status(200).json({ message: "get goals" });
 };
 
 // Set goal
 // route POST /api/goals
 // access Private
-const setGoal = (req, res) => {
+const setGoal = async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
   res.status(200).json({ message: "set goal" });
 };
 
 // Update goal
 // route PUT /api/goals/:id
 // access Private
-const updateGoal = (req, res) => {
+const updateGoal = async (req, res) => {
   res.status(200).json({ message: `update goal ${req.params.id}` });
 };
 
 // Delete goal
 // route DELETE /api/goals/:id
 // access Private
-const deleteGoal = (req, res) => {
+const deleteGoal = async (req, res) => {
   res.status(200).json({ message: `delete goal ${req.params.id}` });
 };
 
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", require("./routes/goalRoutes.js"));
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);
