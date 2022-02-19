be3c753ed96d79006386bd5faa99124edb5ed283
hermanlyx <hermanlyx@gmail.com>
2022-02-19 19:56:55 +0800

    added anync handler to goalController

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,33 +1,35 @@
+const asyncHandler = require("express-async-handler");
+
 // Get goals
 // route GET /api/goals
 // access Private
-const getGoals = async (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "get goals" });
-};
+});
 
 // Set goal
 // route POST /api/goals
 // access Private
-const setGoal = async (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text field");
   }
   res.status(200).json({ message: "set goal" });
-};
+});
 
 // Update goal
 // route PUT /api/goals/:id
 // access Private
-const updateGoal = async (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `update goal ${req.params.id}` });
-};
+});
 
 // Delete goal
 // route DELETE /api/goals/:id
 // access Private
-const deleteGoal = async (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `delete goal ${req.params.id}` });
-};
+});
 
 module.exports = { getGoals, setGoal, updateGoal, deleteGoal };
