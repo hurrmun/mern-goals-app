bab682ef73a26638ec948b476c867fd845a5c048
hermanlyx <hermanlyx@gmail.com>
2022-02-19 20:32:45 +0800

    created goal model, linked to mongoDB database, and updated routes to change items in database

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,26 @@
+//* dependencies
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
-const port = process.env.PORT || 3001;
+const connectDB = require("./config/db");
 
+//* configuration
+const port = process.env.PORT || 3001;
+connectDB();
 const app = express();
 
+//* middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//* routes middleware
 app.use("/api/goals", require("./routes/goalRoutes.js"));
 
+//* error middleware
 app.use(errorHandler);
 
+//* app listener
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);
 });
