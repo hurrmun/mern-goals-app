a68b76c42ad8a92787a4b4381a654630015a3a81
hermanlyx <hermanlyx@gmail.com>
2022-02-24 23:17:10 +0800

    added register (logs you in) and logout functionality

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -15,8 +15,13 @@ const register = async (userData) => {
   return response.data;
 };
 
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
 const authService = {
   register,
+  logout,
 };
 
 export default authService;
