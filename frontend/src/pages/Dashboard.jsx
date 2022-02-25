23b222adf074deeda0256c44898ed4f9c1b0b3f3
hermanlyx <hermanlyx@gmail.com>
2022-02-25 19:08:23 +0800

    dashboard now only shows when logged in, homepage is able to create goals in database

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,28 @@
-const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import GoalForm from "../components/GoalForm";
 
-export default Dashboard
+const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
+  return (
+    <>
+      <section className="heading">
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+      <GoalForm />
+    </>
+  );
+};
+
+export default Dashboard;
