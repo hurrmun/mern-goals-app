5bdb82b7343dfe46f4a19782c378ca07d5efc1f7
hermanlyx <hermanlyx@gmail.com>
2022-03-08 23:02:40 +0800

    added middleware to serve build files in production

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 //* dependencies
+const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
@@ -17,6 +18,17 @@ app.use(express.urlencoded({ extended: false }));
 //* routes middleware
 app.use("/api/goals", require("./routes/goalRoutes.js"));
 app.use("/api/users", require("./routes/userRoutes.js"));
+
+//* Serve frontend
+if (procewss.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build"))); // __dirname = current directory
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  ); // point all routes (besides API routes) to index.html
+}
 
 //* error middleware
 app.use(errorHandler);
